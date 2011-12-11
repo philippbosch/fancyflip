1cbc6514bba249eaf84ce08751a5de7d720590b1
Philipp Bosch <hello@pb.io>
2011-12-11 17:45:37 +0100

    Update app based on 501a6ec9a725d442b72753e9bf8dff15f8abe7de

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,24 @@
 (function() {
-
+  $(document).bind('touchmove', function(e) {
+    e.preventDefault();
+    return false;
+  });
+  $(function() {
+    return $('.flippable').on('webkitAnimationStart webkitAnimationEnd', function(e) {
+      return $(this).toggleClass('animated', e.type.substr(-5).toLowerCase() === 'start');
+    }).on('click', function(e) {
+      var $el;
+      $el = $(this);
+      if ($el.is('.animated')) {
+        return false;
+      }
+      if ($el.is('.flip-forward')) {
+        $el.removeClass('flip-forward');
+        return $el.addClass('flip-backward');
+      } else {
+        $el.removeClass('flip-backward');
+        return $el.addClass('flip-forward');
+      }
+    });
+  });
 }).call(this);
