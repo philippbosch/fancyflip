2b78e6779bceb6f0e408a2a10c7736008c42a9be
Philipp Bosch <hello@pb.io>
2011-12-13 08:42:31 +0100

    Update app based on ec452027843c98392480df385ade08390902a185

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,6 +3,37 @@
     e.preventDefault();
     return false;
   });
+  $.fn.transition = function(properties, duration, ease, callback) {
+    var $el, depth, transitionCallback;
+    if (duration == null) {
+      duration = 400;
+    }
+    depth = parseInt($(this).data('transition-depth') || 0, 10) + 1;
+    $el = $(this);
+    $el.data('transition-depth', depth);
+    transitionCallback = function() {
+      if (properties === "__sleep__") {
+        return window.setTimeout(function() {
+          return $el.trigger("transitionend-" + depth);
+        }, duration);
+      } else {
+        return $el.animate(properties, duration, ease, function() {
+          return $el.trigger("transitionend-" + depth);
+        });
+      }
+    };
+    if (depth === 1) {
+      return transitionCallback();
+    } else {
+      return $el.bind("transitionend-" + (depth - 1), transitionCallback);
+    }
+  };
+  $.fn.transitionSleep = function(duration) {
+    if (duration == null) {
+      duration = 1000;
+    }
+    return $(this).transition('__sleep__', duration);
+  };
   $(function() {
     return $('.flippable').on('webkitAnimationStart webkitAnimationEnd', function(e) {
       return $(this).toggleClass('animated', e.type.substr(-5).toLowerCase() === 'start');
@@ -12,13 +43,28 @@
       if ($el.is('.animated')) {
         return false;
       }
-      if ($el.is('.flip-forward')) {
-        $el.removeClass('flip-forward');
-        return $el.addClass('flip-backward');
-      } else {
-        $el.removeClass('flip-backward');
-        return $el.addClass('flip-forward');
-      }
+      $el.transition({
+        scale: 0.75
+      }, 500);
+      $el.transition({
+        scale: 0.75,
+        rotateY: '180deg'
+      }, 1000);
+      $el.transition({
+        scale: 1,
+        rotateY: '180deg'
+      }, 500);
+      $el.transitionSleep();
+      $el.transition({
+        scale: 0.75,
+        rotateY: '180deg'
+      }, 500);
+      $el.transition({
+        scale: 0.75
+      }, 1000);
+      return $el.transition({
+        scale: 1
+      }, 500);
     });
   });
 }).call(this);
